2064785b95c99dc086201dcf07b737382a123bfa
Chaney1024 <chen0227@outlook.com>
2021-03-21 09:48:21 +0800

    first commit

---
+++ Parser.py
@@ -0,0 +1,140 @@
+# 解析数据，生成提交formData
+class Parser:
+    def __init__(self, data):
+        self.item_keys = {'_VAR_EXECUTE_INDEP_ORGANIZE_Name': None, '_VAR_ACTION_INDEP_ORGANIZES_Codes': None,
+                          '_VAR_ACTION_REALNAME': None, '_VAR_ACTION_ORGANIZE': None, '_VAR_EXECUTE_ORGANIZE': None,
+                          '_VAR_ACTION_INDEP_ORGANIZE': None, '_VAR_ACTION_INDEP_ORGANIZE_Name': None,
+                          '_VAR_ACTION_ORGANIZE_Name': None, '_VAR_EXECUTE_ORGANIZES_Names': None,
+                          '_VAR_OWNER_ORGANIZES_Codes': None, '_VAR_ADDR': None, '_VAR_OWNER_ORGANIZES_Names': None,
+                          '_VAR_URL': None, '_VAR_EXECUTE_ORGANIZE_Name': None, '_VAR_RELEASE': None,
+                          '_VAR_NOW_MONTH': None,
+                          '_VAR_ACTION_USERCODES': None, '_VAR_ACTION_ACCOUNT': None,
+                          '_VAR_ACTION_INDEP_ORGANIZES_Names': None,
+                          '_VAR_OWNER_ACCOUNT': None, '_VAR_ACTION_ORGANIZES_Names': None, '_VAR_STEP_CODE': None,
+                          '_VAR_OWNER_USERCODES': None, '_VAR_EXECUTE_ORGANIZES_Codes': None, '_VAR_NOW_DAY': None,
+                          '_VAR_OWNER_REALNAME': None, '_VAR_NOW': None, '_VAR_URL_Attr': None,
+                          '_VAR_ENTRY_NUMBER': None,
+                          '_VAR_EXECUTE_INDEP_ORGANIZES_Names': None, '_VAR_STEP_NUMBER': None, '_VAR_POSITIONS': None,
+                          '_VAR_EXECUTE_INDEP_ORGANIZES_Codes': None, '_VAR_EXECUTE_POSITIONS': None,
+                          '_VAR_ACTION_ORGANIZES_Codes': None, '_VAR_EXECUTE_INDEP_ORGANIZE': None,
+                          '_VAR_NOW_YEAR': None,
+                          'fieldFLid': None, 'fieldHQRQ': None, 'fieldDQSJ': None, 'fieldSQSJ': None,
+                          'fieldJBXXxm': None,
+                          'fieldJBXXxm_Name': None, 'fieldJBXXgh': None, 'fieldJBXXnj': None, 'fieldJBXXbj': None,
+                          'fieldJBXXxb': None, 'fieldJBXXxb_Name': None, 'fieldJBXXlxfs': None, 'fieldJBXXcsny': None,
+                          'fieldJBXXdw': None, 'fieldJBXXdw_Name': None, 'fieldJBXXbz': None, 'fieldJBXXbz_Name': None,
+                          'fieldJBXXfdy': None, 'fieldJBXXfdy_Name': None, 'fieldjgs': None, 'fieldjgs_Name': None,
+                          'fieldjgshi': None, 'fieldjgshi_Name': None, 'fieldJBXXxnjzbgdz': None, 'fieldJBXXJG': None,
+                          'fieldJBXXjgs': None, 'fieldJBXXjgs_Name': None, 'fieldJBXXjgshi': None,
+                          'fieldJBXXjgshi_Name': None,
+                          'fieldJBXXjgq': None, 'fieldJBXXjgq_Name': None, 'fieldJBXXjgsjtdz': None,
+                          'fieldJBXXdrsfwc': None,
+                          'fieldJBXXsheng': None, 'fieldJBXXshi': None, 'fieldJBXXqu': None, 'fieldJBXXqjtxxqk': None,
+                          'fieldSTQKbrstzk1': None, 'fieldSTQKfs': None, 'fieldSTQKks': None, 'fieldSTQKxm': None,
+                          'fieldSTQKfl': None, 'fieldSTQKhxkn': None, 'fieldSTQKfx': None, 'fieldSTQKqt': None,
+                          'fieldSTQKqtms': None, 'fieldSTQKfrtw': None, 'fieldSTQKfrsj': None, 'fieldSTQKclfs': None,
+                          'fieldSTQKzd': None, 'fieldSTQKbrstzk': None, 'fieldSTQKglfs': None, 'fieldSTQKgldd': None,
+                          'fieldSTQKglkssj': None, 'fieldSTQKzdjgmc': None, 'fieldSTQKzdmc': None,
+                          'fieldSTQKzdkssj': None,
+                          'fieldSTQKzljgmc': None, 'fieldSTQKzysj': None, 'fieldSTQKzdjgmcc': None,
+                          'fieldSTQKpcsj': None,
+                          'fieldSTQKjtcystzk1': None, 'fieldSTQKjtcyfs': None, 'fieldSTQKjtcyks': None,
+                          'fieldSTQKjtcyxm': None,
+                          'fieldSTQKjtcyfl': None, 'fieldSTQKjtcyhxkn': None, 'fieldSTQKjtcyfx': None,
+                          'fieldSTQKjtcyqt': None,
+                          'fieldSTQKjtcyqtms': None, 'fieldSTQKjtcyfrtw': None, 'fieldSTQKjtcyfrsj': None,
+                          'fieldSTQKjtcyclfs': None,
+                          'fieldSTQKjtcyzd': None, 'fieldSTQKjtcystzk': None, 'fieldSTQKjtcyglfs': None,
+                          'fieldSTQKjtcygldd': None,
+                          'fieldSTQKjtcyglkssj': None, 'fieldSTQKjtcyzdjgmc': None, 'fieldSTQKjtcyzdmc': None,
+                          'fieldSTQKjtcyzdkssj': None, 'fieldSTQKjtcyzljgmc': None, 'fieldSTQKjtcyzysj': None,
+                          'fieldSTQKjtcyzdjgmcc': None, 'fieldSTQKjtcypcsj': None, 'fieldSTQKrytsqkqsm': None,
+                          'fieldCXXXszsqsfyyshqzbl': None, 'fieldCXXXqjymsxgqk': None, 'fieldCXXXsfjcgyshqzbl': None,
+                          'fieldCXXXksjcsj': None, 'fieldCXXXzhycjcsj': None, 'fieldJCDDs': None, 'fieldJCDDshi': None,
+                          'fieldJCDDq': None, 'fieldJCDDqmsjtdd': None, 'fieldCXXXjcdr': None, 'fieldCXXXjcdqk': None,
+                          'fieldYQJLsfjcqtbl': None, 'fieldYQJLksjcsj': None, 'fieldYQJLzhycjcsj': None,
+                          'fieldYQJLjcdry': None,
+                          'fieldYQJLjcdds': None, 'fieldYQJLjcddshi': None, 'fieldYQJLjcddq': None,
+                          'fieldYQJLjcdryjkqk': None,
+                          'fieldqjymsjtqk': None, 'fieldCXXXsftjhb': None, 'fieldCXXXsftjhbjtdz': None,
+                          'fieldCXXXsftjhbs': None,
+                          'fieldCXXXsftjhbq': None, 'fieldCXXXddsj': None, 'fieldCXXXsfylk': None,
+                          'fieldCXXXlksj': None,
+                          'fieldCNS': None, 'fieldJKHDDzt': None, 'fieldJKHDDzt_Name': None, 'fieldzgzjzdzq': None,
+                          'fieldzgzjzdzjtdz': None, 'fieldzgzjzdzshi': None, 'fieldzgzjzdzs': None,
+                          'fieldCXXXcxzt': None,
+                          'fieldCXXXjtgjbc': None, 'fieldCXXXjtfsqtms': None, 'fieldCXXXjtfsqt': None,
+                          'fieldCXXXjtfslc': None,
+                          'fieldCXXXjtfspc': None, 'fieldCXXXjtfsdb': None, 'fieldCXXXjtfshc': None,
+                          'fieldCXXXjtfsfj': None,
+                          'fieldCXXXfxcfsj': None, 'fieldCXXXcqwdq': None, 'fieldCXXXdqszd': None, 'fieldCXXXssh': None,
+                          'fieldCXXXfxxq': None, 'fieldCXXXjtjtzz': None, 'fieldCXXXjtzzq': None,
+                          'fieldCXXXjtzzq_Name': None,
+                          'fieldCXXXjtzzs': None, 'fieldCXXXjtzzs_Name': None, 'fieldCXXXjtzz': None,
+                          'fieldCXXXjtzz_Name': None,
+                          'fieldSTQKqtqksm': None, 'fieldSHENGYC': None, 'fieldYCFDY': None, 'fieldYCBZ': None,
+                          'fieldYCBJ': None}
+        self.data = data
+
+    def _finditem(self, obj, key):
+        if key in obj: return obj[key]
+        for k, v in obj.items():
+            if isinstance(v, dict):
+                item = self._finditem(v, key)
+                if item is not None:
+                    return item
+            elif isinstance(v, list):
+                for row in v:
+                    item = self._finditem(row, key)
+                    if item is not None:
+                        return item
+
+    def getData(self):
+        data = {}
+        for k, v in self.item_keys.items():
+            re = self._finditem(self.data, k)
+            if re is not None:
+                data[k] = re
+            else:
+                print("error", k)
+        return data
+
+    def get(self):
+        post_data1 = self.getData()
+        post_data2 = {
+            'fieldJBXXbz_Attr': {"_parent": ""},
+            'fieldJBXXfdy_Attr': {"_parent": "%s" % self.data['entities'][0]['data']['fieldYCFDY']},
+            'fieldjgshi_Attr': {"_parent": "%s" % self.data['entities'][0]['data']['fieldjgs']},
+            'fieldJBXXjgshi_Attr': {"_parent": "%s" % self.data['entities'][0]['data']['fieldJBXXjgs']},
+            'fieldJBXXjgq_Attr': {"_parent": "%s" % self.data['entities'][0]['data']['fieldJBXXjgshi']},
+            'fieldJBXXsheng_Name': '',
+            'fieldJBXXshi_Name': '',
+            'fieldJBXXshi_Attr': {"_parent": ""},
+            'fieldJBXXqu_Name': '',
+            'fieldJBXXqu_Attr': {"_parent": ""},
+            'fieldJCDDs_Name': '',
+            'fieldJCDDshi_Name': '',
+            'fieldJCDDshi_Attr': {"_parent": ""},
+            'fieldJCDDq_Name': '',
+            'fieldJCDDq_Attr': {"_parent": ""},
+            'fieldYQJLjcdds_Name': '',
+            'fieldYQJLjcddshi_Name': '',
+            'fieldYQJLjcddshi_Attr': {"_parent": ""},
+            'fieldYQJLjcddq_Name': '',
+            'fieldYQJLjcddq_Attr': {"_parent": ""},
+            'fieldCXXXsftjhbjtdz_Name': '',
+            'fieldCXXXsftjhbs_Name': '',
+            'fieldCXXXsftjhbs_Attr': {"_parent": ""},
+            'fieldCXXXsftjhbq_Name': '',
+            'fieldCXXXsftjhbq_Attr': {"_parent": ""},
+            'fieldzgzjzdzq_Name': '',
+            'fieldzgzjzdzq_Attr': {"_parent": ""},
+            'fieldzgzjzdzshi_Name': '',
+            'fieldzgzjzdzshi_Attr': {"_parent": ""},
+            'fieldzgzjzdzs_Name': '',
+            'fieldCXXXfxxq_Name': '',
+            '_VAR_ENTRY_NAME': '学生健康状况申报_',
+            '_VAR_ENTRY_TAGS': '疫情应用,移动端'
+        }
+        post_data_info = dict(post_data1, **post_data2)
+        return post_data_info
---
+++ YQTB.py
@@ -0,0 +1,224 @@
+import base64
+import json
+import random
+import re
+import time
+from urllib import parse
+import requests
+from bs4 import BeautifulSoup
+from Parser import Parser
+
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+class YQTB:
+    # 初始化参数
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.csrfToken = ''
+        self.formStepId = ''
+        self.formUrl = ''
+        self.client = requests.session()
+        self.client.headers = {
+            'Proxy-Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, '
+                          'like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,'
+                      '*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,und;q=0.7',
+        }
+
+    # 识别验证码
+    def ocr(self, image):
+        url = "https://api2.chaney.top/release/gzhu"
+        payload = {
+            'key': 'b42fb9486f3c10e8654072f0648e694f',
+            'image': image,
+        }
+        response = requests.post(url, data=payload)
+        return response.json()
+
+    # 获取验证码
+    def captcha(self):
+        logger.info('验证码识别')
+        image = self.client.get(url='https://cas.gzhu.edu.cn/cas_server/captcha.jsp')
+        base_data = base64.encodebytes(image.content)
+        res = self.ocr(base_data)
+        return res['result']
+
+    # 登陆账号
+    def login(self):
+        logger.info('开始登陆')
+        res = self.client.get(url="http://yqtb.gzhu.edu.cn/")
+        soup = BeautifulSoup(res.text, "html.parser")
+        form = soup.find_all('input')
+        post_url = soup.find('form')['action']
+        post_data = {}
+        for row in form:
+            post_data[row['name']] = row['value']
+        del post_data['reset']
+        login_post_url = parse.urljoin(res.url, post_url)
+
+        post_data['username'] = self.username
+        post_data['password'] = self.password
+        post_data['captcha'] = self.captcha()
+        res = self.client.post(url=login_post_url, data=post_data)
+        soup = BeautifulSoup(res.content.decode('utf-8'), 'lxml')
+
+        if soup.title.string != '广州大学':
+            logger.warning('验证码错误，尝试重新登陆')
+            self.login()
+        logger.info('登陆成功')
+
+    # 准备数据
+    def prepare(self):
+        logger.info("准备数据")
+        res = self.client.get(url="http://yqtb.gzhu.edu.cn/infoplus/form/XNYQSB/start?back=1&x_posted=true")
+        soup = BeautifulSoup(res.content.decode('utf-8'), 'lxml')
+        self.csrfToken = soup.find(attrs={"itemscope": "csrfToken"})['content']
+        self.formStepId = re.findall(r"\d+", res.url)[0]
+        self.formUrl = res.url
+        post_data = {
+            'stepId': self.formStepId,
+            'instanceId': '',
+            'admin': 'false',
+            'rand': random.uniform(300, 400),
+            'width': 1440,
+            'lang': 'zh',
+            'csrfToken': self.csrfToken
+        }
+        headers = {
+            'Host': 'yqtb.gzhu.edu.cn',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'X-Requested-With': 'XMLHttpRequest',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Origin': 'http://yqtb.gzhu.edu.cn',
+            'Referer': self.formUrl,
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,und;q=0.7',
+            'Connection': 'close'
+        }
+        res = self.client.post(url="http://yqtb.gzhu.edu.cn/infoplus/interface/render", headers=headers, data=post_data)
+        self.getDatas = res.json()
+
+    # 开始执行打卡
+    def start(self):
+        logger.info("执行打卡")
+        formData = Parser(self.getDatas).get(),
+
+        headers = {
+            'Host': 'yqtb.gzhu.edu.cn',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'X-Requested-With': 'XMLHttpRequest',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Origin': 'http://yqtb.gzhu.edu.cn',
+            'Referer': self.formUrl,
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,und;q=0.7',
+            'Connection': 'close'
+        }
+        post_data1 = {
+            'stepId': self.formStepId,
+            'actionId': 1,
+            'formData': json.dumps(formData[0]),
+            'timestamp': int(time.time()),
+            'rand': random.uniform(700, 800),
+            'boundFields': "fieldSTQKzdjgmc,fieldSTQKjtcyglkssj,fieldCXXXsftjhb,fieldzgzjzdzjtdz,fieldJCDDqmsjtdd,"
+                           "fieldSHENGYC,fieldYQJLksjcsj,fieldSTQKjtcyzd,fieldJBXXjgsjtdz,fieldSTQKbrstzk,"
+                           "fieldSTQKfrtw,fieldSTQKjtcyqt,fieldCXXXjtfslc,fieldJBXXlxfs,fieldSTQKpcsj,fieldJKHDDzt,"
+                           "fieldYQJLsfjcqtbl,fieldYQJLzhycjcsj,fieldSTQKfl,fieldSTQKhxkn,fieldJBXXbz,fieldCXXXsfylk,"
+                           "fieldFLid,fieldjgs,fieldSTQKglfs,fieldCXXXsfjcgyshqzbl,fieldSTQKjtcyfx,"
+                           "fieldCXXXszsqsfyyshqzbl,fieldJCDDshi,fieldSTQKrytsqkqsm,fieldJCDDs,fieldSTQKjtcyfs,"
+                           "fieldSTQKjtcyzljgmc,fieldSQSJ,fieldzgzjzdzs,fieldzgzjzdzq,fieldJBXXnj,"
+                           "fieldSTQKjtcyzdkssj,fieldSTQKfx,fieldSTQKfs,fieldYQJLjcdry,fieldCXXXjtfsdb,fieldCXXXcxzt,"
+                           "fieldYQJLjcddshi,fieldCXXXjtjtzz,fieldCXXXsftjhbs,fieldHQRQ,fieldSTQKjtcyqtms,"
+                           "fieldCXXXksjcsj,fieldSTQKzdkssj,fieldSTQKjtcyzysj,fieldjgshi,fieldSTQKjtcyxm,"
+                           "fieldJBXXsheng,fieldJBXXdrsfwc,fieldqjymsjtqk,fieldJBXXdw,fieldCXXXjcdr,"
+                           "fieldCXXXsftjhbjtdz,fieldJCDDq,fieldSTQKjtcyclfs,fieldSTQKxm,fieldCXXXjtgjbc,"
+                           "fieldSTQKjtcygldd,fieldzgzjzdzshi,fieldSTQKjtcyzdjgmcc,fieldSTQKzd,fieldSTQKqt,"
+                           "fieldCXXXlksj,fieldSTQKjtcyfrsj,fieldCXXXjtfsqtms,fieldSTQKjtcyzdmc,fieldCXXXjtfsfj,"
+                           "fieldJBXXfdy,fieldJBXXxm,fieldSTQKzljgmc,fieldCXXXzhycjcsj,fieldCXXXsftjhbq,"
+                           "fieldSTQKqtms,fieldYCFDY,fieldJBXXxb,fieldSTQKglkssj,fieldCXXXjtfspc,fieldSTQKbrstzk1,"
+                           "fieldYCBJ,fieldCXXXssh,fieldSTQKzysj,fieldJBXXgh,fieldCNS,fieldCXXXfxxq,fieldSTQKclfs,"
+                           "fieldSTQKqtqksm,fieldCXXXqjymsxgqk,fieldYCBZ,fieldJBXXxnjzbgdz,fieldSTQKjtcyfl,"
+                           "fieldSTQKjtcyzdjgmc,fieldCXXXddsj,fieldSTQKfrsj,fieldSTQKgldd,fieldCXXXfxcfsj,"
+                           "fieldJBXXbj,fieldSTQKks,fieldJBXXcsny,fieldCXXXjtzzq,fieldJBXXJG,fieldCXXXdqszd,"
+                           "fieldCXXXjtzzs,fieldJBXXshi,fieldSTQKjtcyfrtw,fieldSTQKjtcystzk1,fieldCXXXjcdqk,"
+                           "fieldSTQKzdmc,fieldSTQKjtcyks,fieldSTQKjtcystzk,fieldCXXXjtfshc,fieldCXXXcqwdq,"
+                           "fieldSTQKjtcypcsj,fieldJBXXqu,fieldJBXXjgshi,fieldYQJLjcddq,fieldYQJLjcdryjkqk,"
+                           "fieldYQJLjcdds,fieldSTQKjtcyhxkn,fieldCXXXjtzz,fieldJBXXjgq,fieldCXXXjtfsqt,fieldJBXXjgs,"
+                           "fieldSTQKzdjgmcc,fieldJBXXqjtxxqk,fieldDQSJ,fieldSTQKjtcyglfs",
+            'csrfToken': self.csrfToken,
+            'lang': 'zh'
+        }
+        post_data2 = {
+            'stepId': self.formStepId,
+            'actionId': 1,
+            'formData': json.dumps(formData[0]),
+            'nextUsers': "{}",
+            'timestamp': int(time.time()),
+            'rand': random.uniform(700, 800),
+            'boundFields': "fieldSTQKzdjgmc,fieldSTQKjtcyglkssj,fieldCXXXsftjhb,fieldzgzjzdzjtdz,fieldJCDDqmsjtdd,"
+                           "fieldSHENGYC,fieldYQJLksjcsj,fieldSTQKjtcyzd,fieldJBXXjgsjtdz,fieldSTQKbrstzk,"
+                           "fieldSTQKfrtw,fieldSTQKjtcyqt,fieldCXXXjtfslc,fieldJBXXlxfs,fieldSTQKpcsj,fieldJKHDDzt,"
+                           "fieldYQJLsfjcqtbl,fieldYQJLzhycjcsj,fieldSTQKfl,fieldSTQKhxkn,fieldJBXXbz,fieldCXXXsfylk,"
+                           "fieldFLid,fieldjgs,fieldSTQKglfs,fieldCXXXsfjcgyshqzbl,fieldSTQKjtcyfx,"
+                           "fieldCXXXszsqsfyyshqzbl,fieldJCDDshi,fieldSTQKrytsqkqsm,fieldJCDDs,fieldSTQKjtcyfs,"
+                           "fieldSTQKjtcyzljgmc,fieldSQSJ,fieldzgzjzdzs,fieldzgzjzdzq,fieldJBXXnj,"
+                           "fieldSTQKjtcyzdkssj,fieldSTQKfx,fieldSTQKfs,fieldYQJLjcdry,fieldCXXXjtfsdb,fieldCXXXcxzt,"
+                           "fieldYQJLjcddshi,fieldCXXXjtjtzz,fieldCXXXsftjhbs,fieldHQRQ,fieldSTQKjtcyqtms,"
+                           "fieldCXXXksjcsj,fieldSTQKzdkssj,fieldSTQKjtcyzysj,fieldjgshi,fieldSTQKjtcyxm,"
+                           "fieldJBXXsheng,fieldJBXXdrsfwc,fieldqjymsjtqk,fieldJBXXdw,fieldCXXXjcdr,"
+                           "fieldCXXXsftjhbjtdz,fieldJCDDq,fieldSTQKjtcyclfs,fieldSTQKxm,fieldCXXXjtgjbc,"
+                           "fieldSTQKjtcygldd,fieldzgzjzdzshi,fieldSTQKjtcyzdjgmcc,fieldSTQKzd,fieldSTQKqt,"
+                           "fieldCXXXlksj,fieldSTQKjtcyfrsj,fieldCXXXjtfsqtms,fieldSTQKjtcyzdmc,fieldCXXXjtfsfj,"
+                           "fieldJBXXfdy,fieldJBXXxm,fieldSTQKzljgmc,fieldCXXXzhycjcsj,fieldCXXXsftjhbq,"
+                           "fieldSTQKqtms,fieldYCFDY,fieldJBXXxb,fieldSTQKglkssj,fieldCXXXjtfspc,fieldSTQKbrstzk1,"
+                           "fieldYCBJ,fieldCXXXssh,fieldSTQKzysj,fieldJBXXgh,fieldCNS,fieldCXXXfxxq,fieldSTQKclfs,"
+                           "fieldSTQKqtqksm,fieldCXXXqjymsxgqk,fieldYCBZ,fieldJBXXxnjzbgdz,fieldSTQKjtcyfl,"
+                           "fieldSTQKjtcyzdjgmc,fieldCXXXddsj,fieldSTQKfrsj,fieldSTQKgldd,fieldCXXXfxcfsj,"
+                           "fieldJBXXbj,fieldSTQKks,fieldJBXXcsny,fieldCXXXjtzzq,fieldJBXXJG,fieldCXXXdqszd,"
+                           "fieldCXXXjtzzs,fieldJBXXshi,fieldSTQKjtcyfrtw,fieldSTQKjtcystzk1,fieldCXXXjcdqk,"
+                           "fieldSTQKzdmc,fieldSTQKjtcyks,fieldSTQKjtcystzk,fieldCXXXjtfshc,fieldCXXXcqwdq,"
+                           "fieldSTQKjtcypcsj,fieldJBXXqu,fieldJBXXjgshi,fieldYQJLjcddq,fieldYQJLjcdryjkqk,"
+                           "fieldYQJLjcdds,fieldSTQKjtcyhxkn,fieldCXXXjtzz,fieldJBXXjgq,fieldCXXXjtfsqt,fieldJBXXjgs,"
+                           "fieldSTQKzdjgmcc,fieldJBXXqjtxxqk,fieldDQSJ,fieldSTQKjtcyglfs",
+            'csrfToken': self.csrfToken,
+            'lang': 'zh'
+        }
+
+        res1 = self.client.post(url='http://yqtb.gzhu.edu.cn/infoplus/interface/listNextStepsUsers', headers=headers,
+                                data=post_data1)
+        res2 = self.client.post(url='http://yqtb.gzhu.edu.cn/infoplus/interface/doAction', headers=headers,
+                                data=post_data2)
+
+        if res1.json()['errno'] or res2.json()['errno']:
+            self.notify('打卡失败')
+        else:
+            self.notify('打卡成功')
+
+    # 消息推送
+    def notify(self, msg):
+        logger.info(msg)
+
+    # 开始运行
+    def run(self):
+        logger.info('开始执行任务')
+        self.login()
+        self.prepare()
+        self.start()
+        logger.info('打卡完成')
+
+
+if __name__ == '__main__':
+    username = 'xxx'  # 学号
+    password = 'xxx'  # 密码
+
+    YQTB(username, password).run()
